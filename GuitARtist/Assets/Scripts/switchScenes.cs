687fd2bc6e2ae4247a3dd7aa518fd20af844ac59
MeenakshiSivapriya <meenakshisivapriya@gmail.com>
2018-03-02 14:23:31 +0530

    Unity UI files

---
+++ GuitARtist/Assets/Scripts/switchScenes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void settings()
+	{
+		Application.LoadLevel ("Settings");
+	}
+
+	public void instructions()
+	{
+		Application.LoadLevel ("Instructions");
+	}
+
+	public void input()
+	{
+		Application.LoadLevel ("Input");
+	}
+
+	public void menu()
+	{
+		Application.LoadLevel ("Main");
+	}
+
+	public void ar()
+	{
+		Application.LoadLevel ("AR");
+	}
+}
