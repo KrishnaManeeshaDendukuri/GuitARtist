e3de22881903835331dd40e5f39e00acda36077e
MeenakshiSivapriya <meenakshisivapriya@gmail.com>
2018-03-02 15:20:47 +0530

    Added socket communication

---
+++ GuitARtist/Assets/Scripts/textSocket.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System;
+
+public class textSocket : MonoBehaviour 
+{
+	TcpClient client;
+	Stream s;
+	StreamReader sr;
+	StreamWriter sw;
+	Thread recvT;
+	public bool ready = false;
+	public bool running = true;
+	public string message;
+	string temp;
+
+	public InputField iField;
+	public string text;
+	public string song;
+	// Use this for initialization
+	void Start () 
+	{
+		new Thread(initThread).Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void initThread()
+	{
+		TcpClient client = new TcpClient("127.0.0.1", 5204);
+		s = client.GetStream();
+		sr = new StreamReader(s);
+		sw = new StreamWriter(s);
+		sw.AutoFlush = true;
+
+		recvT = new Thread(recvThread);
+		recvT.Start();
+
+		ready = true;
+	}
+
+	public void send(string message)
+	{
+		temp = message;
+		new Thread(sendThread).Start();
+	}
+
+	public void sendThread()
+	{
+		try
+		{
+			string name = temp;
+			sw.WriteLine(name);
+		}
+		catch
+		{
+		}
+	}
+
+	public void recvThread()
+	{
+		print("RecvThread started...");
+		while (running)
+		{
+			try
+			{
+				message = sr.ReadLine();
+				print("Recieved : " + message);
+			}
+			catch (Exception e)
+			{
+				print(e);
+			}
+		}
+	}
+
+
+	public void getText()
+	{
+		Debug.Log (iField.text);
+		song = iField.text;
+		send (song);
+	}
+}
