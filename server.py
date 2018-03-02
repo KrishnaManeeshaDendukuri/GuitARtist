166159b8de353ef07207cdfa36ae9762578f9538
MeenakshiSivapriya <meenakshisivapriya@gmail.com>
2018-03-03 02:30:44 +0530

    Web scraping for mp3 files

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-import socket               
+import socket 
+import webScrape             
 
 s = socket.socket()         
 print ("Socket successfully created")
 
-port = 5204              
- 
+port = 5204               
 s.bind(('', port))        
 print ("socket binded to %s" %(port))
  
@@ -22,9 +22,22 @@ while True:
 		if data:
 			data = data.decode().rstrip("\n\r")
 			print("to send "+data, ", length: ", len(data))
-			#if len(data)!=0:
+
+			if len(data)!=0:
+				newData = data.split(',')
+				song_data = newData[0]
+				artist_data = newData[1]
+				webScrape.main(song_data, artist_data)
 		else:
 			break
 	finally:
 		pass
+
+def sendToUnity(chords):
+	if (len(chords)!=0):
+		try:
+			chords = chords + "\n\r"
+			c.send(chords)
+		except:
+			c.close()
 				
---
+++ webScrape.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen,Request,urlretrieve
+import re
+import youtube_dl
+import sys
+import os
+
+def ytscrape(searchurl,baseurl):
+    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
+    lst[:] = []
+    url = urlopen(req)
+    soup = BeautifulSoup(url, 'lxml')
+    for i in soup.find_all('div',{'class':['yt-lockup-content','yt-lockup-meta-info']},limit=10):
+        for link,views in zip(i.select('h3 > a'),i.select('ul > li')):
+            if views is not None and views.next_sibling is not None:
+                lst.append([baseurl+link.get('href'),views.next_sibling.text])
+    for i in lst:
+        i[1] = int(re.sub(r' views|,','',i[1]))
+    lst.sort(key = lambda x:x[1])
+    url.close()
+    return lst[-1][0]
+
+def dl_frm_youtube(yt_lnk,dlpath):
+    """passes the youtube url of the song. it extracts audio alone and saves it
+    in local.
+    yt_lnk : youtube url for song which is priortised based on channel/views.
+    """
+    ydl_opts = {'format':'bestaudio/best','outtmpl':dlpath+'\\%(title)s.%(ext)s','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}]}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(yt_lnk, download=True)
+        songname = info.get('title', None)
+        if os.path.isfile(dlpath+'\\'+songname+'.mp3'):
+            print('found')
+        else:
+            print('not found')
+
+def main(song_name,artist_name):
+    song = str(song_name) 
+    artist = str(artist_name)
+    baseurl = 'https://www.youtube.com'
+    if sys.platform == 'win32':
+        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
+        if not os.path.exists(dlpath):
+            os.mkdir(dlpath)
+    else:
+        dlpath = '~/Music/' + song + '.mp3'
+    searchurl = baseurl + '/results?search_query=' + '+' + artist.replace(chr(32),'+') + '+' + song.replace(chr(32),'+')
+    dl_frm_youtube(ytscrape(searchurl,baseurl),dlpath)
+
+
+lst = []
